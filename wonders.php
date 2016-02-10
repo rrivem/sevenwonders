95e7a3262f707b824159f8326b632ddd602a0a7d
jr <jr@cetyounix.SPINETIX.local>
2016-02-10 22:00:52 +0100

    removed debug code selecting always the same wonder

--- wonders.php
+++ wonders.php
@@ -77,12 +77,6 @@ class SevenWonders {
 
         // set up the start conditions
         $wonderKeys = array_keys($this->wonders);       
-        foreach ( $wonderKeys as $key ){
-            if ( $this->wonders[$key]['name'] == "babylon" ){
-                $cheatkKey = $key;
-            }
-        }
-
         shuffle($wonderKeys);
         foreach($this->players as $player){
             // starting moneyz
@@ -90,7 +84,7 @@ class SevenWonders {
 
             // select a wonder
             $wonder = $this->wonders[array_pop($wonderKeys)];
-            $wonder = $this->wonders[$cheatkKey];
+
             $player->wonderName = $wonder['name'];
         }
 
